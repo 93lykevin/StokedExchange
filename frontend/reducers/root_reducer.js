59cf6839300d9bbe03600a9c9151fb194c81d7e0
kevly <93ly.kevin@gmail.com>
2019-09-09 16:54:17 -0700

    Add errors reducer, session errors reducer, and combined rootReducer

---
+++ frontend/reducers/root_reducer.js
@@ -0,0 +1,13 @@
+import { combineReducers } from 'redux';
+
+import entitiesReducer from './entities_reducer';
+import sessionReducer from './sessoion_reducer';
+import errorsReducer from './errors_reducer';
+
+const rootReducer = combineReducers({
+    entities: entitiesReducer,
+    session: sessionReducer,
+    errors: errorsReducer
+});
+
+export default rootReducer;
