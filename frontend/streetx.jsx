6cea116b1e86ab9cb7f99ccdd9a2992d98fa9665
kevly <93ly.kevin@gmail.com>
2019-09-09 17:30:53 -0700

    Finished phase 1 of BenchBnB auth guide for StreetX. Able to signup/login/logout on the window. Start phase 2

--- frontend/streetx.jsx
+++ frontend/streetx.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { signup, login, logout } from './util/session_api_util';
-
+// import { signup, login, logout } from './util/session_api_util';
+import configureStore from './store/store';
+import { login , logout } from './actions/session_actions'
 
 
 document.addEventListener("DOMContentLoaded", () => {
-    window.signup = signup;
+    // window.signup = signup;
     window.login = login;
     window.logout = logout;
+    const store = configureStore();
+    window.store = store;
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
     const root = document.getElementById("root");
     ReactDOM.render(<h1>Welcome to StreetX</h1>, root);
 });
