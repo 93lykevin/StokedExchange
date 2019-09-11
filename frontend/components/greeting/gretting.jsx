2a6d49f53dea980b3ca43704437fa500b74c64e0
kevly <93ly.kevin@gmail.com>
2019-09-10 19:40:59 -0700

    PRE-Refactor NavBar to its own component and hope it doesn't break

--- frontend/components/greeting/gretting.jsx
+++ frontend/components/greeting/gretting.jsx
@@ -3,9 +3,22 @@ import { Link } from 'react-router-dom';
 
 const Greeting = ({ currentUser, logout }) => {
 
-    // const navBar = () => (
-        
-    // );
+    const navBar = () => (
+        <div className="nav-bar-container">
+            <img src={window.streetxURL} alt="logo" className="logo" />
+            <nav className="nav-bar-list" >
+                <ul>
+                    <li><a href="#">Browse</a></li>
+                    <li><a href="#">Portfolio</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">FAQ</a></li>
+                    <li><a href="#">Account</a></li>
+                    <li><a href="#">Sell</a></li>
+                    <li><button className="header-button" onClick={logout}>Log Out</button></li>
+                </ul>
+            </nav>
+        </div>
+    );
 
     const sessionLinks = () => (
         <nav className="login-signup"> 
@@ -19,20 +32,7 @@ const Greeting = ({ currentUser, logout }) => {
         <hgroup className="header-group">
             <section className="sauce">
                 <h2 className="header-name">Hi, {currentUser.username}! </h2>
-                <button className="header-button" onClick={logout}>Log Out</button>
-                
-                <div className="nav-bar-container">
-                    <img src="/assets/StreetX.png" alt="logo" className="logo"/>
-                    <nav className="nav-bar-list" >
-                        <ul>
-                            <li><a href="#">Browse</a></li>
-                            <li><a href="#">Portfolio</a></li>
-                            <li><a href="#">About</a></li>
-                            <li><a href="#">FAQ</a></li>
-                            <li><a href="#">Account</a></li>
-                        </ul>
-                    </nav>
-                </div>
+                {navBar()}
             </section>
         </hgroup>
     );
