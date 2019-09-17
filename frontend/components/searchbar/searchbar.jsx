de0310f12bfac1486d7ab0ea83c9d8539711be9d
kevly <93ly.kevin@gmail.com>
2019-09-17 11:40:08 -0700

    Finished decent searchbar for now. Only shows 5 products

--- frontend/components/searchbar/searchbar.jsx
+++ frontend/components/searchbar/searchbar.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import * as ProductAPIUtil from '../../util/product_api_util';
-import SearchProduct from './search_product'
+import SearchProduct from './search_product';
+import InfiniteScroll from 'react-infinite-scroller';
 
 class SearchBar extends React.Component{
     constructor(props){
         super(props)
 
         //initial state
-        this.state = { search_val: "", 
-                        products: [] };
+        this.state = { 
+            search_val: "", 
+            products: [],
+            // hasMoreItems: true,
+            // nextHref: null
+        };
 
         this.handleInput = this.handleInput.bind(this);
         this.clearSearch = this.clearSearch.bind(this);
     }
+    
 
     handleInput(e) {
         if (e.currentTarget.value === '') {
@@ -51,10 +57,12 @@ class SearchBar extends React.Component{
             <div className="search-container">
                 <div className='product-search'>
                     <form className="search-form">
+                    <i class="fas fa-search"></i>
                         <input className='nav-search'
                             onChange={this.handleInput}
                             type="text"
                             name="product"
+                            autoComplete="off"
                             placeholder='Search for brand, color, etc.'/>
                         <ul className="search-result-ul">
                             <li className="list-item" onClick={this.clearSearch}>
